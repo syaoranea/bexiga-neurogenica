45d509bf229ef7b30f5f9a72c75bd2d42f3fff7a
syaoranea <eduardo_rocha87@hotmail.com>
2025-10-03 23:03:55 -0300

    feat: google

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,13 +5,14 @@ import { Inter } from 'next/font/google'
 import Header from '@/components/header'
 import Footer from '@/components/footer'
 import { ThemeProvider } from '@/components/theme-provider'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Bexiga Neurogênica - Informações e Apoio',
   description: 'Site completo com informações, tratamentos, cuidados práticos e apoio para pacientes com bexiga neurogênica e seus familiares.',
-  keywords: 'bexiga neurogênica, incontinência urinária, tratamentos, cuidados, apoio, informações médicas',
+  keywords: 'bexiga neurogênica, incontinência urinária, tratamentos, cuidados, apoio, informações médicas, urodinamica, dificuldade em esvaziar bexiga',
 }
 
 export default function RootLayout({
@@ -21,6 +22,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR" suppressHydrationWarning>
+      <head>
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-SLBWJJ97KQ"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-SLBWJJ97KQ');
+          `}
+        </Script>
+      </head>
       <body className={inter.className} suppressHydrationWarning>
         <ThemeProvider
           attribute="class"
